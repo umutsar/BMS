657f9056d4657e89d694d15d979ec6ecf0491c9b
Umut Şar <contact@umutsar.net>
2024-02-28 23:16:12 +0300

    Update arduino-receiver.ino

--- arduino-receiver.ino
+++ arduino-receiver.ino
@@ -2,7 +2,7 @@
 
 byte dataUart[64];
 uint16_t cells[20];
-float sumVoltage, soc, powerWatt, cellsVoltage[20], maxVPosition, minVPosition, differantiePressure,
+uint16_t sumVoltage, soc, powerWatt, cellsVoltage[20], maxVPosition, minVPosition, differantiePressure,
     temps[4], maxTemp, minTemp, meanTemp, current, minVoltage, maxVoltage;
 int count = 0;
 
@@ -13,6 +13,11 @@ void setup()
 
 void loop()
 {
+  
+    for(int i = 0; i < 64; i++) {
+        dataUart[i] = 0;
+    }
+
     count = 0;
     delay(1000);
 
@@ -24,15 +29,15 @@ void loop()
     sumVoltage = (dataUart[0] * 256 + dataUart[1]) / 10;
     soc = (dataUart[2] * 256 + dataUart[3]) / 10;
     powerWatt = (dataUart[4] * 256 + dataUart[5]) / 1000;
-    maxVPosition = dataUart[130];
-    minVPosition = dataUart[134];
-    differantiePressure = dataUart[136];
-    maxTemp = dataUart[108];
-    minTemp = dataUart[110];
-    meanTemp = dataUart[112];
-    current = (dataUart[179] * 256 + dataUart[180]) / 63;
-    maxVoltage = (dataUart[127] * 256 + dataUart[128]) / 100;
-    minVoltage = (dataUart[131] * 256 + dataUart[132]) / 100;
+    maxVPosition = dataUart[48];
+    minVPosition = dataUart[49];
+    differantiePressure = dataUart[50];
+    maxTemp = dataUart[55];
+    minTemp = dataUart[57];
+    meanTemp = dataUart[59];
+    current = (dataUart[4] * 256 + dataUart[5]) / 63;
+    maxVoltage = (dataUart[46] * 256 + dataUart[47]) / 100;
+    minVoltage = (dataUart[62] * 256 + dataUart[63]) / 100;
 
     for (int i = 0; i < 20; i++)
     {
@@ -106,6 +111,7 @@ void loop()
     for (int i = 0; i < 20; i++)
     {
         Serial.print(cells[i]);
+        Serial.print(" ");
     }
     Serial.println(" ");
 }
